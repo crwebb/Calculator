ec813d32f06b0adb6b6324b0b97c4759e05ad5fe
Chris Webb <Christopher-webb@live.co.uk>
2020-11-09 21:55:22 +0000

    Added comments for future refactoring. Fixed issue with pressing equals before anything else and total not working after wards in part of expressions.

--- calculator.js
+++ calculator.js
@@ -30,10 +30,10 @@ let operatorActive = false;
 
 let firstOperand = null;
 let secondOperand = null;
-let total = 0;
+let total = null;
 
 let previousOperator;
-let globalOperator;
+let globalOperator = undefined;
 let lastButton;
 
 
@@ -45,7 +45,7 @@ function enterNum(e) {
 
   if (lastButton === "AC") {
   
-    total = 0;
+    total = null;
     displayValue.textContent = 0;
     displayValue.textContent = "";
     displayDiv.appendChild(displayValue);
@@ -63,6 +63,10 @@ if (operatorActive === false) {
   displayValue.textContent += e.target.textContent;
   };
   console.log("Display Value", e.target.textContent);
+
+  console.log(total, "total")
+  // console.log(firstOperand)
+  console.log(secondOperand, "secondOperand")
 };
 
 
@@ -73,7 +77,7 @@ let symbol = e.target.textContent;
 
 operatorActive = true;
 
-firstOperand = parseFloat(displayValue.textContent);
+secondOperand = parseFloat(displayValue.textContent);
 
 if (symbol === "+") globalOperator = "add";
 if (symbol === "-") globalOperator = "subtract";
@@ -84,15 +88,30 @@ if (symbol === "=") lastButton = "equals";
 
 if (symbol === "+" || symbol === "-" || symbol === "*" || symbol === "/") {
 
-  console.log("Operator Chain Path")
+  /* When an operator is pressed, if second operand is less than total and total === 0,
+   set total to be value of second operand */
+  if (secondOperand > total && total === 0) total = secondOperand;
+
+  console.log("Operator Chain Path");
   equals("OperatorChain");
 
-} else if (symbol === "=") {
+  /* If user presses equals before anything else, 
+  check if globalOperator has changed, set variable values to 0*/
+} else if (symbol === "=" && globalOperator === undefined) {
 
-  console.log("Equals Path")
+  console.log("NO GLOBAL OPERATOR BEFORE EQUALS");
+  displayDummy.remove();
+  displayValue.textContent = "0";
+
   equals();
 
-  };
+} else if (symbol === "=") {
+  console.log("Equals Path");
+  equals();
+  }; 
+
+  console.log(total, "total")
+  console.log(secondOperand, "secondOperand")
 };
  
 
@@ -121,100 +140,97 @@ if (arg1 === "OperatorChain") {
 };
 
 previousOperator = globalOperator;
-displayValue.textContent = total;
+displayValue.textContent = parseFloat(total);
 
-console.log("Total: ", total)
+
+console.log(total, "total");
+console.log(secondOperand, "second operand");
+
 };
 
 
-
-
 function operate(a, b, operator) {
+
+if (globalOperator === undefined) return 0;
+
 if (operator === "add") {
-  return add(a, b);
-  } else if (operator === "subtract") {
-  return subtract(a, b); 
-  } else if (operator === "multiply") {
-  return multiply(a, b);
-  } else if (operator === "divide") {
-  return divide(a, b);
+
+  console.log(total, "total");
+  console.log(secondOperand, "secondOperand");
+
+    return add(a, b);
+} else if (operator === "subtract") {
+    return subtract(a, b); 
+} else if (operator === "multiply") {
+    return multiply(a, b);
+} else if (operator === "divide") {
+    return divide(a, b);
   };
 };
 
 
-
 function add(a, b) {
-  console.log("Add was called: ", "First Opperand:", a, "Second Opperand: ",  b);
+  console.log("Add was called: ", "Total (First Operand)", a, "Second Opperand: ",  b);
   return a + b;
 };
 
 function subtract(a, b) {
-  console.log("Subtract was called: ", "First Opperand:", a, "Second Opperand: ",  b);
+  console.log("Subtract was called: ", "Total (First Operand)", a, "Second Opperand: ",  b);
   if ( a === 0) {
-      return b - a 
+      return b - a   // If total === 0 this stops the - operator making the expression -X
   } else {
       return a - b;
   };
 };
 
 function multiply(a, b) {
-  console.log("Multiply was called: ", "First Opperand:", a, "Second Opperand: ",  b);
-  if (a * b === 0) {
-      return b;
+  console.log("Multiply was called: ", "Total (First Operand)", a, "Second Opperand: ",  b);
+  if (a * b === 0) { // Without this, multiplying by 0 makes the display flick to 0 too early
+      return b; 
   } else { 
       return a * b;
   };
 };
 
 function divide(a, b) {
-  console.log("Divide was called: ", "First Opperand:", a, "Second Opperand: ",  b);
+  console.log("Divide was called: ", "Total (First Operand)", a, "Second Opperand: ",  b);
   if (b === 0) {
-    return "Inappropriate";
-} else if (a / b === 0) { 
+    return "Inappropriate"; // Stops total being divded by 0
+} else if (a / b === 0) {   // Stops total being divded by 0
     return b;
   } else {
     return a / b
   };
 };
 
-
 acBtn.addEventListener("click", allClear);
 function allClear() {
 
+  // Deletes the display value
   lastButton = "AC";
   displayValue.remove();
 
+  // Appends the dispalyDummy <p> element to act as placeholder
   displayDiv.appendChild(displayDummy);
   displayDummy.textContent = 0;
 
-
+  // Reset values of all variables
   globalOperator =  undefined;
   previousOperator = undefined;
-  firstOperand = null;
-  total = 0;
-
-  console.log(displayValue.textContent, "Display Value")
-  console.log(displayDummy.textContent, "Display Dummy")
+  secondOperand = null;
+  total = null;
 };
 
-
-
 /* DELETE BUTTON This works but get the operators to chain first
-
 delBtn.addEventListener("click", deleteChar);
 function deleteChar() {
-
 let displayString = displayValue.textContent;
 let editedDisplayString = displayString.slice(0, -1)
-
 if (editedDisplayString.length === 0) {
-
   displayValue.textContent = 0;
-
 } else if (total !== null && editedDisplayString.length === 0) {
   operatorActive = true;
   displayValue.textContent = total;
-
 } else if (displayValue.textContent === "Inappropriate") {
   operatorActive = true;
   displayValue.textContent = total;
